9de4ee9d89a226b10b4d0dddbc0fac1f9807d4ce
kazacube-amkili <amkili@kazacube.com>
2024-05-29 16:59:07 +0100

    [IMP] Dev Initial fonction bonjour

--- formation.py
+++ formation.py
@@ -1,1 +1,20 @@
-print("Premier developpement")
+def Bonjour(login):
+    utilisateurs = {
+        "Houda@kazacube.com": "Houda",
+        "Ahmed@kazacube.com": "Ahmed",
+        "Rania@kazacube.com": "Rania",
+        "Meftah@kazacube.com": "Meftah",
+        "Marwa@kazacube.com": "Marwa",
+        "Hanna@kazacube.com": "Hanna",
+
+    }
+
+    if login in utilisateurs:
+        nom = utilisateurs[login]
+        print(f"Bonjour {nom}")
+    else:
+        print("Login non reconnu")
+
+
+login_entre = input("Veuillez entrer votre login: ")
+Bonjour(login_entre)
