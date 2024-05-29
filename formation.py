5c99ca8e614e4b6588b1b024006e89136e662071
kazacube-mcheriaa <165287465+kazacube-mcheriaa@users.noreply.github.com>
2024-05-29 17:17:43 +0100

    [IMP] (#2)
    ajout d'un code de print
    
    -------------------------------------------------------------------------
    
    Co-authored-by: Mohammed <mziouadi@kazacube.com>
    Co-authored-by: kazacube-hlemkiri <146965289+kazacube-hlemkiri@users.noreply.github.com>

--- formation.py
+++ formation.py
@@ -1,5 +1,9 @@
 print("Premier developpement")
+print("i'm not really bored, I'm more of frustrated")
+print("it'll get better")
+
 print("hanaa dev")
 
 for i in range(10):
     print("thank you teacher Houda ")
+
